4a5076906988c2f3eb2ded4130ad642ca402ad55
yicheng <yicheng-me@qq.com>
2021-12-02 21:38:18 +0800

    增加css测试页面

---
+++ src/views/css-assets/index.page.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>css-assets测试页面</div>
+
+  <h2 class="test1">我是红色的</h2>
+  <h2 class="test2">我是蓝色的</h2>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.test1 {
+  color: red;
+}
+.test2 {
+  color: blue;
+}
+</style>
